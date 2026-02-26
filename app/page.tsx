7d79be4d935259a037f0e3b759806e47f94517c3
Brad Hendrickson <bradley.hendrickson6@gmail.com>
2026-02-25 21:30:27 -0800

    Revert "Solid background, Layson image above title, less top margin"
    This reverts commit 8cef9786543c8ade1577483cfa5c9ad976a95989.

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,8 @@
-import Image from "next/image";
 import Wheel from "./components/Wheel";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex flex-col items-center justify-start pt-4 px-6 pb-6">
-      <div className="flex justify-center mb-4">
-        <Image
-          src="/images/layson.jpg"
-          alt="Layson"
-          width={280}
-          height={280}
-          className="rounded-full object-cover w-40 h-40 sm:w-52 sm:h-52 md:w-64 md:h-64"
-        />
-      </div>
+    <main className="min-h-screen flex flex-col items-center justify-center p-6">
       <h1 className="text-4xl sm:text-5xl md:text-6xl font-bold text-amber-950 mb-4 text-center">
         Is Layson Right?
       </h1>
